3c9167a06ef59c8ae88f05ea195ea6c42d63aba2
miguel.dinois <miguel.dinois@aluno.senai.br>
2026-09-18 10:39:43 -0300

    feat: estrutura inicial do aplicativo com componentes Header e lógica de consumo de água

--- App.jsx
+++ App.jsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {useState} from 'react';
+import {COLORS} from './src/constants/colors';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
 export default function App() {
+  const META = 2000; 
+  const [consumo, setConsumo] = useState(0);
+
+  const handleAddWater = (amount) => {
+    setConsumo(consumo + amount);
+  }
+
+  const handleReset = () => {
+    setConsumo(0);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Diario de Hidratação</Text>
+      <Header />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+  return (
+    <View>
+      <Text>💧 Diário de Hidratação</Text>
+      <Text>Meta diária: 2000 ml</Text>
+    </View>
+  );
+}
