317d998f3e1566799f500259b304ed865279d9c7
miguel.dinois <miguel.dinois@aluno.senai.br>
2026-09-18 10:05:45 -0300

    feat: Adicionar estrutura e estilos iniciais do aplicativo, remover arquivos não utilizados

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    background: '#F0F9FF',
+    cardBg: '#FFFFFF',
+    primary: '#0284C7',
+    secondary: '#38BDF8',
+    textMain: '#0C4A6E',
+    textMuted: '#64748B',
+    danger: '#EF4444',
+    white: '#FFFFFF'
+};
+
+export default COLORS
